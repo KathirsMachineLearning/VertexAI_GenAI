0b1358bcb86c7253adf9a838cfda7f58999af9d7
Kathirvel Murugan Siva Subramanian <kathirr@live.com>
2024-06-03 13:56:51 +0530

    Update gcp_logging.py

--- gcp/gcp_logging.py
+++ gcp/gcp_logging.py
@@ -1,28 +1,90 @@
+import os
 import logging
 from google.cloud import logging as gcp_logging
-from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
-import os
+from google.cloud.logging.handlers import CloudLoggingHandler
 
 class GCPLoggingHandler:
-    def __init__(self, credentials_path=None):
-        if credentials_path:
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    def __init__(self, project_id, credentials_path=None, logger_name='default_logger', level=logging.DEBUG):
+        """
+        Initialize the GCP logging handler with the GCP project ID and credentials.
+
+        :param project_id: The ID of your GCP project.
+        :param credentials_path: Path to the service account key file (optional).
+        :param logger_name: Name of the logger.
+        :param level: Logging level.
+        """
+        self.project_id = project_id
+        self.credentials_path = credentials_path
+        self.logger_name = logger_name
+        self.level = level
+
+        self.logger = self._create_logger()
+
+    def _create_logger(self):
+        """
+        Create a logger for Google Cloud Logging.
+
+        :return: A logger object configured for Google Cloud Logging.
+        """
+        if self.credentials_path:
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.credentials_path
 
         # Create a Google Cloud Logging client
-        self.client = gcp_logging.Client()
+        client = gcp_logging.Client(project=self.project_id)
 
         # Create a handler using the client
-        self.handler = CloudLoggingHandler(self.client)
+        handler = CloudLoggingHandler(client)
 
-        # Set up logging with the handler
-        setup_logging(self.handler)
-
-    def get_logger(self, logger_name='default_logger', level=logging.DEBUG):
-        logger = logging.getLogger(logger_name)
-        logger.setLevel(level)
+        # Get the logger
+        logger = logging.getLogger(self.logger_name)
+        logger.setLevel(self.level)
         
         # Check if the handler is already added to prevent duplicate handlers
         if not any(isinstance(h, CloudLoggingHandler) for h in logger.handlers):
-            logger.addHandler(self.handler)
+            logger.addHandler(handler)
         
         return logger
+
+    def log_info(self, message):
+        """
+        Log an info level message.
+
+        :param message: The message to log.
+        """
+        self.logger.info(message)
+        print(message)
+
+    def log_debug(self, message):
+        """
+        Log a debug level message.
+
+        :param message: The message to log.
+        """
+        self.logger.debug(message)
+        print(message)
+
+    def log_warning(self, message):
+        """
+        Log a warning level message.
+
+        :param message: The message to log.
+        """
+        self.logger.warning(message)
+        print(message)
+
+    def log_error(self, message):
+        """
+        Log an error level message.
+
+        :param message: The message to log.
+        """
+        self.logger.error(message)
+        print(message)
+
+    def get_logger(self):
+        """
+        Get the logger instance.
+
+        :return: The logger instance.
+        """
+        return self.logger
